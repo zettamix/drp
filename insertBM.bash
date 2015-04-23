d8ca2312e4b17e5e418f95b39c883c78f73e291d
zettamix <alopez@bluemessaging.com>
2015-04-23 11:11:34 -0500

    primera version de script de migracion, acceso remoto a servidor DRP pendiente.

---
+++ insertBM.bash
@@ -0,0 +1,156 @@
+#!/bin/bash
+#Used files
+f2="fills2.sql"
+m2="messages2.sql"
+mc="messageCompare.sql"
+mcr="rmessageCompare.txt"
+mcrc="$(cat rmessageCompare.txt)"
+fc="fillsCompare.sql"
+fcr="rfillsCompare.txt"
+fcrc="$(cat rfillsCompare.txt)"
+fcne="columnsfillsnameextract.txt"
+fcnef="fcnefinal.txt"
+cfcnef="$(cat fcnefinal.txt)"
+mcne="columnsmessagesnameextract.txt"
+mcnef="mcnefinal.txt"
+cmcnef="$(cat mcnefinal.txt)"
+#prod host
+dbh=""
+#prod user
+dbu=""
+#prod pass
+dbp=""
+#photos local path
+localpath="/home/zubu/test/amazonsynch/"
+#s3 target bucket
+destbucket="s3://bmdrp-test"
+
+
+#wget *ON HOLD UNTIL DRP HAS REMOTE ACCESS*
+
+insertfills2 () {
+echo"Drop, create and inserting Fills2 on Prod"
+mysql -ptoor -Dbmp_2_8 < $f2
+wait
+sleep 2
+echo "Done"
+sleep 2
+echo ""
+}
+
+insertmessa2 () {
+echo "Drop, create and insert Messages2 on Prod"
+mysql -ptoor -Dbmp_2_8 < $m2
+wait
+sleep 2
+echo "Done"
+sleep 2
+echo ""
+}
+
+comparef () {
+echo "Comparing structure between fills and fills2"
+sleep 2
+mysql -ptoor -Dbmp_2_8 < $fc > $fcr
+wait
+if [ "no differences" == "$fcrc" ]; then
+echo "Structure of tables fills and fills2 are the same, lets continue"
+sleep 2
+else
+echo "Structure of tables fills and fills2 don't have the same structure please verify to continue"
+sleep 2
+exit 0
+fi
+echo ""
+}
+
+comparem () {
+echo "Comparing structure between messages and messages2"
+sleep 2
+mysql -ptoor -Dbmp_2_8 < $mc > $mcr
+wait
+if [ "no differences" == "$mcrc" ]; then
+echo "Structure of tables messages and messages2 are the same, lets continue"
+sleep 2
+else
+echo "Structure of Tables messages and messages2 don't have the same structure please verify to continue"
+sleep 2
+exit 0
+fi
+echo ""
+}
+
+insertmessageprod () {
+echo "Process to insert in to messages in Prod environment started"
+sleep 2
+echo "Building query to insert"
+sleep 2
+mysql -h$dbh -u$dbu -p$dbp -Dbmp_2_8 -e"select column_name from information_schema.columns where table_schema='bmp_2_8' and table_name='messages';" > $mcne
+wait
+awk 'NR>2' $mcne | tr '\n' ',' | sed '$s/.$//' > $mcnef
+wait
+echo "DONE, Query ready to be inserted"
+sleep 2
+echo "Inserting rows into messages in Prod environment"
+mysql -h$dbh -u$dbu -p$dbp -Dbmp_2_8 -e"insert into messages select id=NULL as id,"$cmcnef" from messages2;"
+wait
+echo "DONE, messages inserted successfully"
+sleep 2
+echo ""
+}
+
+insertfillsprod () {
+echo "Process to insert in to fills in Prod environment started"
+sleep 2
+echo "Building query to insert"
+sleep 2
+mysql -h$dbh -u$dbu -p$dbp -Dbmp_2_8 -e"select column_name from information_schema.columns where table_schema='bmp_2_8' and table_name='fills';" > $fcne
+wait
+awk 'NR>2' $fcne | tr '\n' ',' | sed '$s/.$//' > $fcnef
+wait
+echo "DONE, Query ready to be inserted"
+mysql -h$dbh -u$dbu -p$dbp -Dbmp_2_8 -e"insert into fills select id=NULL as id,"$cfcnef" from fills2;"
+wait
+echo "Insert DONE"
+sleep 2
+echo "Updating each fill with their new source_id"
+mysql -h$dbh -u$dbu -p$dbp -Dbmp_2_8 -e"update fills as f,(select m.id,m.name from messages as m,messages2 as mdos where m.name=mdos.name) as mm set f.source_id=mm.id where mm.name=f.name;"
+wait
+echo "DONE, fills inserted successfully"
+sleep 2
+echo ""
+}
+
+uploadaws () {
+echo "Getting photos from server"
+sleep 2
+#Pendin until to have remote access to the server
+echo "Uploading media to s3"
+sleep 2
+s3cmd sync $path $destbucket
+wait
+echo "All DONE"
+echo ""
+}
+
+continue () {
+#Just a continue function
+echo ""
+echo "Continue? y/n"
+echo ""
+read ans
+if [ "$ans" = "n" ]; then
+echo "Installation incomplete BYE"
+exit 0
+else
+echo ""
+echo "=============================Lest CONTINUE=================================="
+fi
+}
+
+alld () {
+#Just bye
+echo ""
+echo "=============================ALL DONE BYE=================================="
+}
+
--- migrateFromDrp.bash
+++ migrateFromDrp.bash
@@ -1,3 +1,19 @@
 #!/bin/bash
+path="/home/infonavit/migration"
+zpath="/home/infonavit/nojejs/img/"
 
-echo "Hello"
+#Please (set the execution of the script as an alias and) run it as needed
+
+mysql -uroot -ptoor -Dbmp_2_8 -e"create table if not exists fills2 like fills;"
+
+mysql -uroot -ptoor -Dbmp_2_8 -e"create table if not exists messages2 like messages;"
+
+insert into fills2 select * from fills where altuser=bmp and last;
+
+insert into messages2 select * from messages where id in (select source_id from fills2);
+
+mysqldump --opt --host=bm28reportsrr.cjaguzeo9pm4.us-east-1.rds.amazonaws.com --user=alopez --password=lopez03a --no-create-info --lock-tables=false --where="id=49898819" bmp_2_8 messages2 > $path/messages2.sql
+
+mysqldump --opt --host=bm28reportsrr.cjaguzeo9pm4.us-east-1.rds.amazonaws.com --user=alopez --password=lopez03a --no-create-info --lock-tables=false --where="source_id=49898819" bmp_2_8 fills2 > $path/fills2.sql
+
+zip -r $zpath $path/img.zip *
