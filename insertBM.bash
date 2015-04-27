1b086c273e624670d435f9fb68af203b12fa6113
zettamix <chavaro_alg@hotmail.com>
2015-04-27 10:44:36 -0500

    last changes in insertfills2 mysql command

--- insertBM.bash
+++ insertBM.bash
@@ -28,7 +28,7 @@ destbucket="s3://bmdrp-test"
 
 insertfills2 () {
 echo"Drop, create and inserting Fills2 on Prod"
-mysql -ptoor -Dbmp_2_8 < $f2
+mysql -u$dbu -p$dbp -Dbmp_2_8 < $f2
 wait
 sleep 2
 echo "Done"
